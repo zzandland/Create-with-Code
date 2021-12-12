ad4446ef192e4f0f87768dd7f14d6e880037964a
Si Yong Kim <zzandland@gmail.com>
2021-12-12 01:33:01 -0800

    finish challenge 1

---
+++ prototype_1/Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+{
+  Vector3 propellerRotation = new Vector3();
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void LateUpdate()
+  {
+    transform.Rotate(new Vector3(0, 0, 360) * Time.deltaTime);
+  }
+}
--- prototype_1/Assets/Scripts/PlayerController.cs
+++ prototype_1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+  private float speed = 20.0f;
+  private float turnSpeed = 45.0f;
+  private float horizontalInput;
+  private float forwardInput;
   // Start is called before the first frame update
   void Start()
   {
@@ -13,7 +17,11 @@ public class PlayerController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    // Move the vehicle forward
-    transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    horizontalInput = Input.GetAxis("Horizontal");
+    forwardInput = Input.GetAxis("Vertical");
+    // Move the vehicle forward based on vertical input
+    transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+    // Rotates the car based on horizontal input
+    transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
   }
 }
