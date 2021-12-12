01546cfd226ac6d87d7dfe2062074c02e8c5b376
Si Yong Kim <zzandland@gmail.com>
2021-12-12 02:12:04 -0800

    bonus features 1 - hard complete

--- prototype_1/Assets/Scripts/FollowPlayer.cs
+++ prototype_1/Assets/Scripts/FollowPlayer.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
   public GameObject player;
-  private Vector3 offset = new Vector3(0, 7, -20);
+  private Vector3 behindOffset = new Vector3(0, 7, -20);
+  private Vector3 frontOffset = new Vector3(0, 1.7f, 1.2f);
+  private bool isFrontView = false;
   // Start is called before the first frame update
   void Start()
   {
 
   }
 
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Keypad0))
+    {
+      isFrontView = !isFrontView;
+    }
+  }
+
   // Update is called once per frame
   void LateUpdate()
   {
-    transform.position = player.transform.position + offset;
+    transform.position = player.transform.position + (isFrontView ? frontOffset : behindOffset);
   }
 }
