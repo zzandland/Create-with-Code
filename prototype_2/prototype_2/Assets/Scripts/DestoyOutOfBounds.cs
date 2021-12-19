2856771c61b31c878387150d07291a757746c455
Si Yong Kim <zzandland@gmail.com>
2021-12-19 02:08:43 -0800

    2.2 complete

---
+++ prototype_2/prototype_2/Assets/Scripts/DestoyOutOfBounds.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoyOutOfBounds : MonoBehaviour
+{
+  private float topBound = 30;
+  private float bottomBound = -10;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (transform.position.z > topBound || transform.position.z < bottomBound)
+    {
+      Destroy(gameObject);
+    }
+  }
+}
--- prototype_2/prototype_2/Assets/Scripts/PlayerController.cs
+++ prototype_2/prototype_2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
   public float horizontalInput;
   public float speed = 10.0f;
   public float xRange = 10;
+
+  public GameObject projectilePrefab;
   // Start is called before the first frame update
   void Start()
   {
@@ -28,5 +30,11 @@ public class PlayerController : MonoBehaviour
 
     horizontalInput = Input.GetAxis("Horizontal");
     transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      // Launch a projectile from the player
+      Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+    }
   }
 }
