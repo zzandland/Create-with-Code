7b2b61bcb5ca73b2941a9b2d58425568c4bff05b
Si Yong Kim <zzandland@gmail.com>
2021-12-22 01:32:02 -0800

    challenge 2 complete

---
+++ prototype_2/prototype_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+  public GameObject dogPrefab;
+
+  // Update is called once per frame
+  void Update()
+  {
+    // On spacebar press, send dog
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      // Delete existing dog and send a new one
+      Destroy(GameObject.FindGameObjectWithTag("Dog"));
+      Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+  }
+}
